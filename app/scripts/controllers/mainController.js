8d9a158101a465b094e4e6a1d26215b4f39075e7
hui1hui2hui3 <shao1232868@sina.com>
2015-10-11 15:21:06 +0800

    init app generated by yeoman ionic-gulp generator

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Recordtime.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('Recordtime')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Recordtime.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('Recordtime')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
